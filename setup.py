c8785cb718bfaaecaf73473bfd29a270082ea136
wicklyg <wicklyjr@gmail.com>
2024-01-16 20:13:03 +0900

    modified: setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
@@ -8,6 +9,12 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
     name='mlproject',
